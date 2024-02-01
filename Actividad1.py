b7a4ebc2c219d4a21b5e4582abad2fde8fb833cc
KSofiaPerez14 <soffprz14@gmail.com>
2024-01-31 21:21:34 -0600

    mensaje secreto

--- Actividad1.py
+++ Actividad1.py
@@ -1,8 +1,22 @@
 from cryptography.fernet import Fernet
 
-claveM=input("ingresa tu token")
+print("mensaje enviado")
+llave = Fernet.generate_key()
+
+#Generamos la instancia de Fernet
+f=Fernet(llave)
+
+print("Llave", llave)
+#Encriptamos el mensaje
+token=f.encrypt(b'te quiero marianita')
+print("Token",token)
+
+
+print("mensaje recibido")
+
+claveM=input("Ingresa tu token: ")
 fM=Fernet(claveM)
-msj = input("ingresa msj encryptado")
+msj = input("Ingresa msj encryptado:")
 msj_decry=fM.decrypt(msj)
 
 print(msj_decry)
